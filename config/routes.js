4ffa6d62f2a53ff5bc62493201b49d7f85a8c354
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-09-30 13:12:11 -0500

    server running on PORT: 3001

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,91 @@
-var express = require("express");
-var router = express.Router();
-var db = require("./models");
-var request = require("request"); //Makes http calls
-var cheerio = require("cheerio");
+var scrape = require("../scrape/scrape");
+var Article = require("../models/Article");
+var Note = require("../models/Note");
 
 
+module.exports = function (router) {
 
+    router.get("/", function (req, res) {
+        Article.find({ saved: false }, function (error, found) {
 
-module.exports = router;
+            if (error) {
+                console.log(error);
+            } else if (found.length === 0) {
+                res.render("empty")
+            } else {
+                var hbsObject = {
+                    articles: found
+                };
+                res.render("index", hbsObject);
+            }
+        });
+    });
+
+router.get("/api/fetch/", function (req, res) {
+
+    articlesController.fetch(function (err, docs) {
+
+        if (!docs || docs.inserted.Count === 0) {
+            res.json({ message: "No new articles today. Check back tomorrow!" });
+        }
+        else {
+            res.json({ message: "Added " + docs.insertedCount + " new articles!" });
+        }
+
+    });
+});
+
+router.get("/saved", function (req, res) {
+
+    articlesController.get({ saved: true }, function (data) {
+        var hbsObject = {
+            articles: data
+        };
+        res.render("saved", hbsObject);
+    })
+});
+
+router.patch("/api/articles", function (req, res) {
+
+    articlesController.update(req.body, function (err, data) {
+
+        res.json(data);
+    });
+});
+
+router.get('/notes/:id', function (req, res) {
+
+    Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .exec(function (error, doc) {
+            if (error) console.log(error);
+            else {
+                res.json(doc);
+            }
+        });
+});
+
+router.post('/notes/:id', function (req, res) {
+
+    var newNote = new Note(req.body);
+    newNote.save(function (err, doc) {
+        if (error) console.log(error);
+
+        Article.findOneAndUpdate(
+            { _id: req.params.id },
+            { $push: { note: doc_id } },
+            { new: true },
+            function (err, newdoc) {
+                if (err) console.log(err);
+                res.send(newdoc);
+            });
+    });
+});
+router.get('/deleteNote/:id', function (req, res) {
+    Note.remove({ "_id": req.params.id }, function (err, newdoc) {
+        if (error) console.log(error);
+        res.redirect('/saved');
+    });
+});
+
+};
--- server.js
+++ server.js
@@ -2,41 +2,47 @@
 var express = require("express");
 var bodyParser = require("body-parser"); //JSON responses
 var mongoose = require("mongoose"); //Mongo object modelling
-var db = require("./models"); // requiring models
-var request = require("request"); //Makes http calls
 var exphbs = require("express-handlebars"); //express handlebars
 var cheerio = require("cheerio"); //Scraper
 
-// PORT configuration to the localhost & Heroku
-var PORT = process.env.PORT || process.argv[2] || 27017;
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+mongoose.Promise = Promise;
 
+// PORT configuration to the localhost & Heroku
 // Iniatialize express
 var app = express();
 
 //body-Parser for submits
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
 // Handlebars
-var exphbs = require ("express-handlebars");
+app.use(express.static("public"));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Use express.static for public as directory
-app.use(express.static("public"));
 
 // Controllers
-var router = require("./config/routes.js");
+var router = express.Router();
+
 // Connect to Mongo DB
+require("./config/routes")(router);
+
+
+// // MONGO db
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoosescraper";
+
+// //Connecting to the Mongo db
+// mongoose.connect(MONGODB_URI);
 app.use(router);
 
-// MONGO db
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoosescraper";
-
-//Connecting to the Mongo db
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+var port = process.env.PORT || 3001;
 
 // Start the server
-app.listen(PORT, function () {
-    console.log(`This application is running on port: ${PORT}`);
+app.listen(port, function () {
+    console.log("This application is running on port " + port);
 });
